62b0e502bb2fd4cd6f4de943b5a22da9adbc3676
Rafael <rafael.amtx@gmail.com>
2020-04-10 19:07:45 -0300

    view produtos criada

--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -1,43 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { Image, View, Text, FlatList, TouchableOpacity } from 'react-native'
 
+import api from '../../services/api';
+
 import logoImg from '../../assets/logo.png';
 import imgSabonete from '../../assets/sabonete.jpg';
+
 import styles from './styles';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function Produtos() {
+    const [produtos, setProdutos] = useState([]);
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(0);
+    const [carregando, setCarregando] = useState(false);
+
+
+    async function carregarProdutos() {
+        
+        if (carregando) {
+            return;
+        }
+
+        if (total > 0 && produtos.length == total) {
+            return;
+        }
+
+        setCarregando(true);
+
+        const response = await api.get('produtos', {
+            params: {page}
+        });
+        setProdutos([...produtos, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setCarregando(false);
+    }
+    
+    useEffect( () => {
+        carregarProdutos();
+    }, []);
+
     return (
         <View style={styles.container}>
+            <Image source={logoImg} style={styles.imgLogo}/> 
             <View style={styles.header}>
-                <Image source={logoImg} />
-                <Text style={styles.headerText}>
-                    PAGINA PRODUTOS
-                </Text>
-                <Text style={styles.title}>Bem Vindo!</Text>
-                <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+                <TextInput placeholder="Procure seu produto aqui" style={styles.inputSearch}/>
+                <TouchableOpacity>
+                    <Feather name="search" style={styles.headerSearch} size={20} />
+                </TouchableOpacity>
             </View>
 
             <FlatList
-                data={[1,2,3,4,5,6,7,8,9,10]}
+                data={produtos}
+                keyExtractor={produto => String(produto.id)}
                 showsVerticalScrollIndicator={true}
-                renderItem={() => (
+                onEndReached={carregarProdutos}
+                onEndReachedThreshold={0.2}
+                style={styles.produtoLista}
+                renderItem={({ item: produto}) => (
                     <View style={styles.produto}>
-                        <Image source={imgSabonete} style={styles.produtoImg}></Image>
+                        <Image source={{ uri: produto.imgUrl }}
+                            style={styles.produtoImg}></Image>
                         <View style={styles.textoProduto}>
-                            <Text style={styles.textoProdutoTitulo} >Título</Text>
-                            <Text>Descricao</Text>
-                            <TouchableOpacity
-                                style={styles.botaoDetalhes}
-                                onPress={() => (console.log('apertou'))}>
-                                <Text>Ver mais detalhes</Text>
-                                <Feather name="arrow-right"/>
-                            </TouchableOpacity>
+                            <Text style={styles.textoProdutoTitulo}>{produto.titulo}</Text>
+                            <Text style={styles.textoProdutoDescricao}>{produto.descricao}</Text>
+                            <View style={styles.viewBotaoDetalhes}> 
+                                <TouchableOpacity
+                                    style={styles.botaoDetalhes}
+                                    onPress={() => (console.log('apertou'))}>
+                                    <Text style={styles.buttonDetalhes}>Ver mais detalhes</Text>
+                                    <Feather name="arrow-right" style={styles.iconeDetalhes} />
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>    
                 )}
-
             />
         </View>
     );
+    
 }
--- src/pages/Produtos/styles.js
+++ src/pages/Produtos/styles.js
@@ -5,19 +5,37 @@ export default StyleSheet.create({
 
     container: {
         flex: 1,
-        backgroundColor: '#f7f2ef',
-        paddingTop: Constants.statusBarHeight + 10
+        backgroundColor: '#836FFF',
+        flexDirection: 'column',
+        paddingTop: Constants.statusBarHeight + 10,
     },
-     
-    headerText: {
-        justifyContent: 'center',
-        alignItems: 'center'
+    
+    imgLogo: {
+        marginLeft: 30
+    },
+
+    header: {
+        justifyContent:"flex-end",
+        flexDirection: 'row'
+    },
+    
+    headerSearch: {
+        justifyContent: 'flex-end',
+        paddingTop: 5,
+        paddingRight: 15
+    },
+
+    inputSearch: {
+        color: '#F8F8FF'
     },
 
     produto: {
         flex: 1,
         flexDirection: 'row',
-        width: '100%',
+        padding: 15,
+        backgroundColor: '#FFFAFA',
+        margin: 10,
+        borderRadius: 15,
     },
 
     produtoImg: {
@@ -26,16 +44,31 @@ export default StyleSheet.create({
     },
 
     textoProduto: {
-        paddingTop: 10,
-        width: '100%',
+        paddingTop: 5,
+        marginLeft: 15,
     },
     
     textoProdutoTitulo: {
-        fontSize: 18,
+        fontSize: 22,
     },
     
     textoProdutoDescricao: {
+        fontSize: 18,
+    },
 
+    botaoDetalhes: {
+        flexDirection: 'row',
+        paddingTop: 20,
+       justifyContent: 'flex-end'
+    },
+
+    iconeDetalhes: {
+        paddingTop: 5,
+
+    },
+
+    produtoLista: {
+        width: '100%'
     }
 
 });
